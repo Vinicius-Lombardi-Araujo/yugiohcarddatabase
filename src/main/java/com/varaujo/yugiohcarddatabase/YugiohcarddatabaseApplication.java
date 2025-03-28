66b34e6d26865eaacfcb08069f1eb42eb829b9c9
vinicius <vini_laraujo@hotmail.com>
2025-03-28 16:54:57 -0300

    First commit

---
+++ src/main/java/com/varaujo/yugiohcarddatabase/YugiohcarddatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.varaujo.yugiohcarddatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YugiohcarddatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YugiohcarddatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/varaujo/yugiohcarddatabase/YugiohcarddatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varaujo.yugiohcarddatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YugiohcarddatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
