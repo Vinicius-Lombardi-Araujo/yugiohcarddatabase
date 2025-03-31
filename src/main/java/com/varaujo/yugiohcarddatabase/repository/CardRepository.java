55e86030a7e88f7507e976b9a72d965ab439e186
vinicius <vini_laraujo@hotmail.com>
2025-03-31 19:23:06 -0300

    Create CardRepository

--- src/main/java/com/varaujo/yugiohcarddatabase/repository/CardRepository.java
+++ src/main/java/com/varaujo/yugiohcarddatabase/repository/CardRepository.java
@@ -1,7 +1,9 @@
-package com.varaujo.yugiohcarddatabase;
+package com.varaujo.yugiohcarddatabase.repository;
 
 import com.varaujo.yugiohcarddatabase.domain.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
 }
