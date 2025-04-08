5537b613171489d7aba5f142dad3560091633731
vinicius <vini_laraujo@hotmail.com>
2025-04-08 12:25:37 -0300

    Add Repository annotation

--- src/main/java/com/varaujo/yugiohcarddatabase/repository/SetCardRepository.java
+++ src/main/java/com/varaujo/yugiohcarddatabase/repository/SetCardRepository.java
@@ -2,6 +2,8 @@ package com.varaujo.yugiohcarddatabase.repository;
 
 import com.varaujo.yugiohcarddatabase.domain.SetCard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SetCardRepository extends JpaRepository<SetCard, Long> {
 }
--- src/main/java/com/varaujo/yugiohcarddatabase/repository/SetRepository.java
+++ src/main/java/com/varaujo/yugiohcarddatabase/repository/SetRepository.java
@@ -2,6 +2,8 @@ package com.varaujo.yugiohcarddatabase.repository;
 
 import com.varaujo.yugiohcarddatabase.domain.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SetRepository extends JpaRepository<Set, Long> {
 }
