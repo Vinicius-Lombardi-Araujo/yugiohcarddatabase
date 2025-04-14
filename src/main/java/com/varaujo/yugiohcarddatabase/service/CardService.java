2c6760c1606d59da530744f55fe039b3cc96d08a
vinicius <vini_laraujo@hotmail.com>
2025-04-14 12:42:25 -0300

    Remove unused methods on CardService

--- src/main/java/com/varaujo/yugiohcarddatabase/service/CardService.java
+++ src/main/java/com/varaujo/yugiohcarddatabase/service/CardService.java
@@ -4,18 +4,16 @@ import com.varaujo.yugiohcarddatabase.domain.Card;
 import com.varaujo.yugiohcarddatabase.domain.enums.CardType;
 import com.varaujo.yugiohcarddatabase.repository.CardRepository;
 import io.micrometer.common.util.StringUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CardService {
 
     private final CardRepository cardRepository;
-
-    public CardService(CardRepository cardRepository) {
-        this.cardRepository = cardRepository;
-    }
 
     public Card save(Card card) {
         return cardRepository.save(card);
@@ -23,16 +21,6 @@ public class CardService {
 
     public List<Card> findAll() {
         return cardRepository.findAll();
-    }
-
-    public Card findById(Long id) throws Exception {
-        return cardRepository.findById(id)
-                .orElseThrow(Exception::new);
-    }
-
-    public Card findByName(String name) throws Exception {
-        return cardRepository.findByName(name)
-                .orElseThrow(Exception::new);
     }
 
     public Card update(Long id, Card card) throws Exception {
